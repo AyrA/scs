edceb1c7b01804ce664e4f78da1babd071da492c
Kevin Gut <kevin.gut@bitm.sg>
2017-11-11 06:50:01 +0100

    scs Application Description

--- scs/Properties/AssemblyInfo.cs
+++ scs/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("scs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# scripting Engine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AyrA")]
 [assembly: AssemblyProduct("scs")]
