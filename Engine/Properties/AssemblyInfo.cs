1498c7d59e088679139d5a0c0727374d7aca3b5c
Kevin Gut <kevin.gut@bitm.sg>
2017-11-11 06:49:32 +0100

    DLL Description

--- Engine/Properties/AssemblyInfo.cs
+++ Engine/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("scs Engine Components")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AyrA")]
 [assembly: AssemblyProduct("Engine")]
